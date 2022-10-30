5e855408d456b4d9fe529068aea8d71d08b7ad8a
strotlog <49286967+strotlog@users.noreply.github.com>
2022-10-30 16:28:37 -0700

    build based on commit 00735f2

---
+++ precache-manifest.abd5e3b5d4d129d631ab6257895f90a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aefae57b829de498be13",
+    "url": "./static/css/main.367ccfb1.chunk.css"
+  },
+  {
+    "revision": "aefae57b829de498be13",
+    "url": "./static/js/main.aefae57b.chunk.js"
+  },
+  {
+    "revision": "815eed804768332ab9ac",
+    "url": "./static/js/1.815eed80.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7ac4d93196274bc9647f716c0b54d053",
+    "url": "./index.html"
+  }
+];
